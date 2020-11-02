147e9fdf115fa22ee0d396941890a5a0228a832e
pan8090 <48321581+pan8090@users.noreply.github.com>
2020-11-02 16:21:18 +0800

    Delete tensorflow.py

--- tensorflow.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import tensorflow
-messsage=tf.constant('welcome to the exciting world of Deep Neural Networks!')
-with tf.Session() as sess:
-    print(sess.run(messsage).decode())
